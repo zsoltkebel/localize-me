09d745abfbf568574b625670eb19eaca43259053
Zsolt Kébel <kebel.zsolt@gmail.com>
2021-02-05 11:15:50 +0000

    first commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:localize_me/screens/authenticate/authenticate.dart';
+import 'package:localize_me/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //TODO return either Home or Authenticate widget
+    return Authenticate();
+  }
+}
