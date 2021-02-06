310bd4bd4767dbe0be90abae170c96653847b929
Zsolt Kébel <kebel.zsolt@gmail.com>
2021-02-06 00:22:44 +0000

    Anonymus sign in and sign out working

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:localize_me/model/user.dart';
 import 'package:localize_me/screens/authenticate/authenticate.dart';
 import 'package:localize_me/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    //TODO return either Home or Authenticate widget
-    return Authenticate();
+    final user = Provider.of<User>(context);
+    print(user);
+
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:localize_me/model/user.dart' as custom;
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create custom User object based on Firebase User
+  custom.User _customUserFromFirebaseUser(User user) {
+    return user != null ? custom.User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<custom.User> get user {
+    return _auth.authStateChanges()
+        // .map((User user) => _customUserFromFirebaseUser(user));
+        .map(_customUserFromFirebaseUser); // does the same as above
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _customUserFromFirebaseUser(user);
     } catch(e) {
       print(e.toString());
       return null;
@@ -21,4 +34,12 @@ class AuthService {
   // register with email & password
 
   // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
